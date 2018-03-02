98cf1e05095d20021b98206c1f85ec696d1cb063
Smorgon <35105422+Smorgon@users.noreply.github.com>
2018-03-03 00:26:14 +0300

    change incorrect process

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 module.exports = function longestConsecutiveLength(array) {
-  let len = array.length;
-	let max = 0;
-	let count = 1;
+  array = array.sort((a,b) => {return a-b;});
 
-	for (let i = 1; i < len; i++) {
-		if (array[i] === array[i-1]) {
-			count++;
-		} else {
-			if (count > max) {
-				max = count;
-			}
-			count = 1;
-		}
-	}
+    let len = array.length;
+    let max = 0;
+    let count = 1;
+    if (len === 1) {
+        max = count;
+    }
+    for (let i = 1; i < len; i++) {
+        if (array[i] === (array[i-1] + 1)) {
+            count++;
+        } else if (array[i] === (array[i-1])) {
+
+        } else {
+            if (count > max) {
+                max = count;
+            }
+            count = 1;
+        }
+    }
+
+    return max;
 }
-
-return max;
