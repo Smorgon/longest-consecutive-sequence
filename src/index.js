9539d2ca295d94d62c7fd22d74f9edb726b29d40
Smorgon <35105422+Smorgon@users.noreply.github.com>
2018-03-02 10:12:24 +0200

    Finding Longest Consecutive Length

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  let len = array.length;
+	let max = 0;
+	let count = 1;
+
+	for (let i = 1; i < len; i++) {
+		if (array[i] === array[i-1]) {
+			count++;
+		} else {
+			if (count > max) {
+				max = count;
+			}
+			count = 1;
+		}
+	}
 }
+
+return max;
